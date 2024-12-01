94f86d1ab03ca2adfdbd69273f475e68860c6df9
Lilli-Terry <78546584+Lilli-Terry@users.noreply.github.com>
2024-12-01 17:06:25 -0500

    asdf

--- Assets/Code/For Horor/Entity out.cs
+++ Assets/Code/For Horor/Entity out.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 
 public class Entityout : MonoBehaviour
 {
     public bool triggered;
     public GameObject wall;
+    public TextMeshProUGUI textMeshPro;
 
     private void Start()
     {
+        textMeshPro.gameObject.SetActive(false);
         triggered = false;
     }
     private void OnTriggerEnter(Collider other)
@@ -19,6 +22,23 @@ public class Entityout : MonoBehaviour
         {
             wall.active = false;
             triggered = true;
+            StartCoroutine(message());
         }
+    }
+
+    private IEnumerator message()
+    {
+        PauseMenu.isPaused = true;
+        textMeshPro.gameObject.SetActive(true);
+        textMeshPro.text = "*Something has opened....*";
+        yield return new WaitForSeconds(4f);
+        textMeshPro.text = "Boo: Oh hey Joe! Didn't know you were still here!.....";
+        yield return new WaitForSeconds(4f);
+        textMeshPro.text = "Boo: Why do you look so mad......";
+        yield return new WaitForSeconds(4f);
+        textMeshPro.text = "Boo: Hey I don't mean to scare you but I think we should just keep walking and not turn around anymore.";
+        yield return new WaitForSeconds(5f);
+        textMeshPro.gameObject.SetActive(false) ;
+        PauseMenu.isPaused = false;
     }
 }
